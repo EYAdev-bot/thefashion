565263bed6b0f0a1d2d93c6d4988806386f03425
ekanga <ekangay18@gmail.com>
2025-09-25 17:31:38 +0200

    14 commit

--- functions/blog.func.php
+++ functions/blog.func.php
@@ -3,17 +3,8 @@ function get_post()
 {
     global $conn;
 
-    $req = $conn->query("SELECT  post.id,
-                                post.title,
-                                post.image,
-                                post.date,
-                                post.content,
-                                admins.names
-                                FROM post
-                                JOIN admins
-                                ON post.writer=admins.emails
-                                WHERE posted='1'
-                                ORDER BY date DESC
+    $req = $conn->query("SELECT  * FROM post
+                        ORDER BY date DESC
                                 
                     ");
     $req->execute();
--- functions/home.func.php
+++ functions/home.func.php
@@ -2,18 +2,11 @@
 function get_post() {
     global $conn;
 
-    $req= $conn->query("SELECT  post.id,
-                                post.title,
-                                post.image,
-                                post.date,
-                                post.content,
-                                admins.names
+    $req= $conn->query("SELECT *
                                 FROM post
-                                JOIN admins
-                                ON post.writer=admins.emails
-                                WHERE posted='1'
-                                ORDER BY date DESC
                                 
+                                ORDER BY date DESC
+                                LIMIT 0,5
                     ");
     $req->execute();
 
